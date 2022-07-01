00bfc9687922948c0f65355f953f6db8b1d220ba
hailaz <739476267@qq.com>
2022-07-01 12:53:27 +0800

    test: update

--- adapter_test.go
+++ adapter_test.go
@@ -64,7 +64,34 @@ func Test_New(t *testing.T) {
 	method := http.MethodGet
 	t.Logf("\nuser:%v\npath:%v\nmethod:%v", user, path, method)
 	t.Logf("delete user premission:%v", Enforcer.DeletePermissionsForUser(user))
-	t.Logf("check user premission:%v", Enforcer.Enforce(user, path, method))
-	t.Logf("add user premission:%v", Enforcer.AddPolicy(user, "*", ACTION_ALL))
-	t.Logf("check user premission again:%v", Enforcer.Enforce(user, path, method))
+	CheckPremission(t, user, path, method)
+	AddPremission(t, user, "*", ACTION_ALL)
+	CheckPremission(t, user, path, method)
+
+	user1 := "hailaz"
+	path1 := "/api/v1/*"
+	checkPathTrue := "/api/v1/user/list"
+	checkPathFalse := "/api/v2/user/list"
+	AddPremission(t, user1, path1, ACTION_GET)
+	CheckPremission(t, user1, checkPathTrue, ACTION_POST)
+	CheckPremission(t, user1, checkPathFalse, http.MethodGet)
+	CheckPremission(t, user1, checkPathTrue, http.MethodGet)
+}
+
+// CheckPremission description
+//
+// createTime: 2022-07-01 11:34:19
+//
+// author: hailaz
+func CheckPremission(t *testing.T, user string, path string, method string) {
+	t.Logf("check \tuser[%s] \tpremission[%s] \tpath[%s] \tallow[%v]", user, method, path, Enforcer.Enforce(user, path, method))
+}
+
+// Add description
+//
+// createTime: 2022-07-01 12:19:16
+//
+// author: hailaz
+func AddPremission(t *testing.T, user string, path string, method string) {
+	t.Logf("add \tuser[%s] \tpremission[%s] \tpath[%s] \tresult[%v]", user, method, path, Enforcer.AddPolicy(user, path, method))
 }
